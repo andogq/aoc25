f6650d5f64a099efe9136aebd2c0f2445e024338
Tom Anderson <tom@ando.sh>
2025-12-01 16:57:40 +1100

    rust day1 part 2

--- rust/src/bin/01.rs
+++ rust/src/bin/01.rs
@@ -28,7 +28,35 @@ pub fn part_one(input: &str) -> Option<u64> {
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                let dir = line.chars().next().unwrap();
+                let num = line[1..].parse::<i64>().unwrap();
+
+                num * match dir {
+                    'L' => -1,
+                    'R' => 1,
+                    dir => panic!("unknown dir {dir}"),
+                }
+            })
+            .fold((50i64, 0), |(mut accum, mut count), dir| {
+                // Count full rotations.
+                count += dir.unsigned_abs() / 100;
+
+                let end = accum + (dir % 100);
+
+                if accum != 0 && (end <= 0 || end > 99) {
+                    count += 1;
+                }
+
+                accum = (end + 100) % 100;
+
+                (accum, count)
+            })
+            .1,
+    )
 }
 
 #[cfg(test)]
@@ -44,6 +72,17 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
+    }
+
+    #[test]
+    fn extra() {
+        // assert_eq!(part_two("R1000"), Some(10));
+        assert_eq!(part_two("L50\nR100"), Some(2));
+    }
+
+    #[test]
+    fn mini() {
+        assert_eq!(part_two("L68"), Some(1));
     }
 }
