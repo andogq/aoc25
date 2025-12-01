475273e8a95e877278fd52013240fe194d21470e
Tom Anderson <tom@ando.sh>
2025-12-01 16:07:39 +1100

    rust day1 part1

--- rust/src/bin/01.rs
+++ rust/src/bin/01.rs
@@ -1,7 +1,30 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    Some(
+        input
+            .lines()
+            .map(|line| {
+                let dir = line.chars().next().unwrap();
+                let num = line[1..].parse::<i64>().unwrap();
+
+                num * match dir {
+                    'L' => -1,
+                    'R' => 1,
+                    dir => panic!("unknown dir {dir}"),
+                }
+            })
+            .fold((50, 0), |(mut accum, mut count), dir| {
+                accum = (accum + (dir + 100)) % 100;
+
+                if accum == 0 {
+                    count += 1;
+                }
+
+                (accum, count)
+            })
+            .1,
+    )
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
@@ -15,7 +38,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3));
     }
 
     #[test]
